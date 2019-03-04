2aa68f69e159913588953b64094b1c5c1c724caf
yilmale <yilmale@auburn.edu>
2019-03-04 15:49:20 -0600

    Reflexive Agent model

--- src/main/scala/AOPMain.scala
+++ src/main/scala/AOPMain.scala
@@ -5,6 +5,7 @@ object AOPMain extends App  {
 
   case class Locked(d: String) extends Fact
   case class Acquired(r: String) extends Fact
+  case class State(x: Int, y: Int) extends Fact
 
 
   println("AOP main driver")
@@ -12,14 +13,15 @@ object AOPMain extends App  {
 
   import RuleGenerator._
 
-
+  var a : Int =10
   var b = ReflexAgent (
       Facts(Set(
          Locked("D1"),
          Acquired("R1"),
          Acquired("R3")))
-    ) subjectTo (
-         "Rule1" -- Locked("D1") & Acquired("R1") |--> (Locked("D1"),not(Locked("D3"))),
+      ) subjectTo (
+         "Rule1" -- Locked("D1") & State(a,a) & Acquired("R1") |--> (Locked("D1"),
+                State(a+10,a),not(Locked("D3"))),
          "Rule2" -- Locked("D1") & Acquired("R2") |--> (Locked("D1"),Locked("D3"))
     )
 
